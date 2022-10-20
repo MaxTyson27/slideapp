b6cb16b454df8d8b01ef9329d59d2ef826d186c7
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2022-10-20 21:16:20 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+  
+});
+
+
+const disablePictureModeOnVideo = () => {
+  const video =document.getElementById("video")
+  video.disablePictureInPicture = true
+}
+// disablePictureModeOnVideo()
+
